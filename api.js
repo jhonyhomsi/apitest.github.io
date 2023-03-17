5b72c20a2fc620a613855efdc2e1cac540e6d60b
Jhony10-bit <johnny.aj.antonio@gmail.com>
2023-03-17 04:02:58 +0200

    Add

--- api.js
+++ api.js
@@ -19,7 +19,7 @@ app.post("/data", function (req, res) {
 	const workbook = xlsx.utils.book_new();
 	const sheet = xlsx.utils.json_to_sheet([data]);
 	xlsx.utils.book_append_sheet(workbook, sheet, `${name}`);
-	xlsx.writeFile(workbook, 'data.xlsx');
+	xlsx.writeFile(workbook, `${name}.xlsx`);
 	res.send('Form submitted successfully!');
 });
 
